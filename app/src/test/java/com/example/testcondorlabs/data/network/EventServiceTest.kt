27c17ec9539df58f0c3bff0f320eb674ff315d51
yesiddiaz15 <yesiddiaz15@gmail.com>
2021-09-21 16:09:22 -0500

    Add unit test and import new libraries

---
+++ app/src/test/java/com/example/testcondorlabs/data/network/EventServiceTest.kt
@@ -0,0 +1,4 @@
+package com.example.testcondorlabs.data.network
+
+class EventServiceTest {
+}
---
+++ app/src/test/java/com/example/testcondorlabs/data/network/TeamServiceTest.kt
@@ -0,0 +1,12 @@
+package com.example.testcondorlabs.data.network
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class TeamServiceTest {
+
+    @Test
+    fun getTeams() {
+    }
+}
