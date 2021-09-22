064601a4ee1020624f983ad83050735837d866d5
yesiddiaz15 <yesiddiaz15@gmail.com>
2021-09-22 12:43:42 -0500

    Add button retry when not connect internet

--- app/src/main/java/com/example/testcondorlabs/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/testcondorlabs/ui/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.testcondorlabs.ui.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testcondorlabs.databinding.ActivityMainBinding
 import com.example.testcondorlabs.ui.adapter.ViewPagerAdapter
@@ -18,12 +19,39 @@ class MainActivity : AppCompatActivity() {
         val adapterTabs = ViewPagerAdapter(supportFragmentManager, lifecycle)
         binding.viewPager.adapter = adapterTabs
 
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-            when (position) {
-                0 -> tab.text = "Spanish League"
-                1 -> tab.text = "Premier League"
-                2 -> tab.text = "French League"
-            }
-        }.attach()
+        refreshMain()
+
+        binding.btnRetry.setOnClickListener {
+            refreshMain()
+        }
+    }
+
+    private fun refreshMain(){
+        if(isOnlineNet()){
+            binding.linearMain.visibility = View.VISIBLE
+            binding.relativeRetry.visibility = View.GONE
+            TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+                when (position) {
+                    0 -> tab.text = "Spanish League"
+                    1 -> tab.text = "Premier League"
+                    2 -> tab.text = "French League"
+                }
+            }.attach()
+        }else{
+            binding.linearMain.visibility = View.GONE
+            binding.relativeRetry.visibility = View.VISIBLE
+        }
+    }
+
+    private fun isOnlineNet(): Boolean {
+        try {
+            val process: Process = Runtime.getRuntime().exec("ping -c 1 www.google.es")
+            val int = process.waitFor()
+            return (int == 0);
+
+        } catch (e: Exception) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
