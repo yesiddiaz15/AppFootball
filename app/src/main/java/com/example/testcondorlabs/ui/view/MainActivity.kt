36c3ff1db53c96efaf7ff92ad0c52eb830f7d095
yesiddiaz15 <yesiddiaz15@gmail.com>
2021-09-22 12:04:24 -0500

    Change colors themes

--- app/src/main/java/com/example/testcondorlabs/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/testcondorlabs/ui/view/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.example.testcondorlabs.ui.view
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
-import com.example.testcondorlabs.data.model.TeamsModel
 import com.example.testcondorlabs.databinding.ActivityMainBinding
-import com.example.testcondorlabs.databinding.FragmentSpanishLeagueBinding
-import com.example.testcondorlabs.ui.adapter.TeamAdapter
 import com.example.testcondorlabs.ui.adapter.ViewPagerAdapter
-import com.example.testcondorlabs.ui.viewmodel.TeamViewModel
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
@@ -24,18 +18,11 @@ class MainActivity : AppCompatActivity() {
         val adapterTabs = ViewPagerAdapter(supportFragmentManager, lifecycle)
         binding.viewPager.adapter = adapterTabs
 
-        TabLayoutMediator(binding.tabLayout, binding.viewPager){
-            tab, position ->
-            when(position){
-                0 -> {
-                    tab.text = "Spanish League"
-                }
-                1 -> {
-                    tab.text = "Premier League"
-                }
-                2 -> {
-                    tab.text = "French League"
-                }
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Spanish League"
+                1 -> tab.text = "Premier League"
+                2 -> tab.text = "French League"
             }
         }.attach()
     }
